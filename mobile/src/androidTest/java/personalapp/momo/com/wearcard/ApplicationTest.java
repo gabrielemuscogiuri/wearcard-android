00c040613ec40768b6b6c1b2af0d9c8798068627
Mohamed <mohamed.eddaakouri@gmail.com>
2015-11-14 13:10:03 +0100

    First commit

---
+++ mobile/src/androidTest/java/personalapp/momo/com/wearcard/ApplicationTest.java
@@ -0,0 +1,13 @@
+package personalapp.momo.com.wearcard;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ wear/src/main/java/personalapp/momo/com/wearcard/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package personalapp.momo.com.wearcard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("personalapp.momo.com.wearcard.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
